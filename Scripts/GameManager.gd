33d56db5b32bdb77ee96910c323c6d7745112ed5
Lehfel <felixlehoux01@gmail.com>
2026-05-28 23:15:51 -0400

    Patch Escape

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -5,6 +5,18 @@ var players = {}
 func _ready():
 	multiplayer.peer_connected.connect(_on_peer_connected)
 	multiplayer.peer_disconnected.connect(_on_peer_disconnected)
+	multiplayer.server_disconnected.connect(_on_server_disconnected)
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"): 
+		var current_scene = get_tree().current_scene.name
+		if current_scene == "MainMenu":
+			return 
+			
+		if multiplayer.multiplayer_peer != null:
+			rpc_quit_to_menu.rpc()
+		else:
+			reset_and_go_to_menu()
 
 func _on_peer_connected(id):
 	if multiplayer.is_server():
@@ -15,6 +27,11 @@ func _on_peer_disconnected(id):
 	if multiplayer.is_server():
 		players.erase(id)
 		sync_to_clients()
+		
+		rpc_quit_to_menu.rpc()
+
+func _on_server_disconnected():
+	reset_and_go_to_menu()
 
 func sync_to_clients():
 	rpc_update_players.rpc(players)
@@ -22,3 +39,16 @@ func sync_to_clients():
 @rpc("authority", "call_local", "reliable")
 func rpc_update_players(server_players):
 	players = server_players
+
+@rpc("any_peer", "call_local", "reliable")
+func rpc_quit_to_menu():
+	reset_and_go_to_menu()
+
+func reset_and_go_to_menu():
+	players.clear()
+	
+	if multiplayer.multiplayer_peer != null:
+		multiplayer.multiplayer_peer.close()
+		multiplayer.multiplayer_peer = null
+		
+	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
